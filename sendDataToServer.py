c5f85fa77898ed1901d3b8e0b7c1fa15c44da56f
Jake <juliorodriguezvasquez@gmail.com>
2017-04-13 15:49:36 -0500

    fixed sendDataToServer
    Now works with absolute directory passed in
    Also made it so it returns the number of successfully sent records.

--- sendDataToServer.py
+++ sendDataToServer.py
@@ -5,21 +5,18 @@ import os.path
 #address is URL of the api the data is being sent to
 #pathToJSONDirectory is the full path to the folder which the JSON files are stored in
 def sendData(serverAddress, pathToJSONDirectory):
-	fileCounter = 0;
-	while os.path.exists('data'+str(fileCounter)+'.json') & os.path.isfile('data'+str(fileCounter)+'.json'):
-		if(pathToJSONDirectory[-1] != '/')
+	fileCounter = 0
+	numRecsSentSuccessfully = 0
+	if pathToJSONDirectory[-1] != '/':
 			pathToJSONDirectory = pathToJSONDirectory + '/'
+	while os.path.exists(pathToJSONDirectory+'data'+str(fileCounter)+'.json') & os.path.isfile(pathToJSONDirectory+'data'+str(fileCounter)+'.json'):
 		with open(pathToJSONDirectory + 'data'+str(fileCounter)+'.json') as data_file:
 		    data = json.load(data_file)
 
 		for x in range(0, len(data["timestamps"])):
 			thisRecord = {"time":data["timestamps"][x],"address":data["addresses"][x],"deviceID":data["deviceID"]}
 			resp = requests.post(serverAddress, json=thisRecord)#json=thisRecord)
-			if resp.status_code != 200:
-				print('Failed '+str(x))
-				print(resp.status_code)
-			else:
-				print('Success '+str(x))
-			print str(resp.content)
-
+			if resp.status_code == 200:
+				numRecsSentSuccessfully+=1
 		fileCounter+=1
+	return numRecsSentSuccessfully
